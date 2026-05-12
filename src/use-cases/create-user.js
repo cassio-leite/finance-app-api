be1d3ca2951731afc5b6e7dc4c33ff35d516a4a8
Cássio Leite <cassiocarmo77@gmail.com>
2026-05-12 15:57:46 -0300

    fix: ensure created user is returned on create user repository

--- src/use-cases/create-user.js
+++ src/use-cases/create-user.js
@@ -1,14 +1,14 @@
-import { v4 as uuidv4 } from 'uuid'
+import crypto from 'crypto'
 import bcrypt from 'bcrypt'
 
-import { PostgresCreateUserRepository } from '../repositories/postgres/create-user'
+import { PostgresCreateUserRepository } from '../repositories/postgres/create-user.js'
 
 export class CreateUserUseCase {
     async execute(createUserParams) {
         // TODO: verificar se o email já existe
 
         // Gerar um ID único para o usuário
-        const userId = uuidv4()
+        const userId = crypto.randomUUID()
 
         // criptografar a senha do usuário
         const hashedPassword = await bcrypt.hash(createUserParams.password, 10)
