1ca677ecaddde43ad71e25df4993091a8c89251c
Cássio Leite <cassiocarmo77@gmail.com>
2026-05-24 17:30:11 -0300

    feat: add dependency injection to user routes

--- index.js
+++ index.js
@@ -6,13 +6,26 @@ import {
     GetUserByIdController,
     UpdateUserController,
 } from './src/controllers/index.js'
+import { PostgresGetUserByIdRepository } from './src/repositories/postgres/get-user-by-id.js'
+import { GetUserByIdUseCase } from './src/use-cases/get-user-by-id.js'
+import { PostgresCreateUserRepository } from './src/repositories/postgres/create-user.js'
+import { CreateUserUseCase } from './src/use-cases/create-user.js'
+import { PostgresGetUserByEmailRepository } from './src/repositories/postgres/get-user-by-email.js'
+import { UpdateUserUseCase } from './src/use-cases/update-user.js'
+import { PostgresUpdateUserRepository } from './src/repositories/postgres/update-user.js'
+import { PostgresDeleteUserRepository } from './src/repositories/postgres/delete-user.js'
+import { DeleteUserUseCase } from './src/use-cases/delete-user.js'
 
 const app = express()
 
 app.use(express.json())
 
 app.get('/api/users/:userId', async (request, response) => {
-    const getUserByIdController = new GetUserByIdController()
+    const getUserByIdRepository = new PostgresGetUserByIdRepository()
+
+    const getUserByIdUseCase = new GetUserByIdUseCase(getUserByIdRepository)
+
+    const getUserByIdController = new GetUserByIdController(getUserByIdUseCase)
 
     const { statusCode, body } = await getUserByIdController.execute(request)
 
@@ -20,7 +33,16 @@ app.get('/api/users/:userId', async (request, response) => {
 })
 
 app.post('/api/users', async (request, response) => {
-    const createUserController = new CreateUserController()
+    const createUserRepository = new PostgresCreateUserRepository()
+
+    const createUserByEmailRepository = new PostgresGetUserByEmailRepository()
+
+    const createUserUseCase = new CreateUserUseCase(
+        createUserByEmailRepository,
+        createUserRepository,
+    )
+
+    const createUserController = new CreateUserController(createUserUseCase)
 
     const { statusCode, body } = await createUserController.execute(request)
 
@@ -28,7 +50,16 @@ app.post('/api/users', async (request, response) => {
 })
 
 app.patch('/api/users/:userId', async (request, response) => {
-    const updateUserController = new UpdateUserController()
+    const updateUserRepository = new PostgresUpdateUserRepository()
+
+    const getUserByEmailRepository = new PostgresGetUserByEmailRepository()
+
+    const updateUserUseCase = new UpdateUserUseCase(
+        getUserByEmailRepository,
+        updateUserRepository,
+    )
+
+    const updateUserController = new UpdateUserController(updateUserUseCase)
 
     const { statusCode, body } = await updateUserController.execute(request)
 
@@ -36,7 +67,11 @@ app.patch('/api/users/:userId', async (request, response) => {
 })
 
 app.delete('/api/users/:userId', async (request, response) => {
-    const deleteUserController = new DeleteUserController()
+    const deleteUserRepository = new PostgresDeleteUserRepository()
+
+    const deleteUserUseCase = new DeleteUserUseCase(deleteUserRepository)
+
+    const deleteUserController = new DeleteUserController(deleteUserUseCase)
 
     const { statusCode, body } = await deleteUserController.execute(request)
 
--- src/use-cases/create-user.js
+++ src/use-cases/create-user.js
@@ -1,41 +1,32 @@
 import crypto from 'crypto'
 import bcrypt from 'bcrypt'
-
-import {
-    PostgresCreateUserRepository,
-    PostgresGetUserByEmailRepository,
-} from '../repositories/postgres/index.js'
 import { EmailAlreadyInUseError } from '../errors/user.js'
 
 export class CreateUserUseCase {
+    constructor(getUserByEmailRepository, createUserRepository) {
+        this.getUserByEmailRepository = getUserByEmailRepository
+        this.createUserRepository = createUserRepository
+    }
+
     async execute(createUserParams) {
-        const postgresGetUserByEmailRepository =
-            new PostgresGetUserByEmailRepository()
         const userWithProvidedEmail =
-            await postgresGetUserByEmailRepository.execute(
-                createUserParams.email,
-            )
+            await this.getUserByEmailRepository.execute(createUserParams.email)
 
         if (userWithProvidedEmail) {
             throw new EmailAlreadyInUseError(createUserParams.email)
         }
 
-        // Gerar um ID único para o usuário
         const userId = crypto.randomUUID()
 
-        // criptografar a senha do usuário
         const hashedPassword = await bcrypt.hash(createUserParams.password, 10)
 
-        // inserir o usuário no banco de dados
         const user = {
             ...createUserParams,
             id: userId,
             password: hashedPassword,
         }
 
-        // chamar o repositório
-        const postgresCreateUserRepository = new PostgresCreateUserRepository()
-        const createdUser = await postgresCreateUserRepository.execute(user)
+        const createdUser = await this.createUserRepository.execute(user)
         return createdUser
     }
 }
