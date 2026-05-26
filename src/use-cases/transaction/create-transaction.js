6c7331c50258a1858ffd96b70de463d341f3423e
Cássio Leite <cassiocarmo77@gmail.com>
2026-05-26 13:13:53 -0300

    feat: add CreateTransactionUseCase

---
+++ src/use-cases/transaction/create-transaction.js
@@ -0,0 +1,27 @@
+import crypto from 'crypto'
+import { UserNotFoundError } from '../../errors/user.js'
+
+export class CreateTransactionUseCase {
+    constructor(createTransactionRepository, getUserByIdRepository) {
+        this.createTransactionRepository = createTransactionRepository
+        this.getUserByIdRepository = getUserByIdRepository
+    }
+
+    async execute(createTransactionParams) {
+        const userId = createTransactionParams.userId
+        const user = await this.getUserByIdRepository.execute(userId)
+
+        if (!user) {
+            throw new UserNotFoundError(userId)
+        }
+
+        const transactionId = crypto.randomUUID()
+
+        const transaction = await this.createTransactionRepository.execute({
+            ...createTransactionParams,
+            id: transactionId,
+        })
+
+        return transaction
+    }
+}
