cea36721fb739b990953a43a269c570332dbff3a
Cássio Leite <cassiocarmo77@gmail.com>
2026-06-16 13:34:45 -0300

    test: ensure 400 is returned if password is less than 6 chars

--- src/controllers/user/create-user.test.js
+++ src/controllers/user/create-user.test.js
@@ -125,4 +125,24 @@ describe('Create User Controller', () => {
         // assert
         expect(result.statusCode).toBe(400)
     })
+
+    it('should return 400 if password is less than 6 characters', async () => {
+        // arrange
+        const createUserUseCase = new CreateUserUseCaseStub()
+        const createUserController = new CreateUserController(createUserUseCase)
+        const httpRequest = {
+            body: {
+                first_name: 'Cassio',
+                last_name: 'Leite',
+                email: 'cassio@gmail.com',
+                password: '123',
+            },
+        }
+
+        // act
+        const result = await createUserController.execute(httpRequest)
+
+        // assert
+        expect(result.statusCode).toBe(400)
+    })
 })
